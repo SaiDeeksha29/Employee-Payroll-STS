4ec4a3decd10bc87d0716c2230e3a1adf051a882
SaiDeeksha29 <saideeksha29@gmail.com>
2020-12-14 00:14:11 +0530

    Added:Exception handler when employee id is not found

---
+++ src/main/java/com/example/employeepayrollapp/exceptions/EmployeePayrollException.java
@@ -0,0 +1,10 @@
+package com.example.employeepayrollapp.exceptions;
+
+public class EmployeePayrollException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public EmployeePayrollException(String message) {
+		super(message);
+	}
+}
