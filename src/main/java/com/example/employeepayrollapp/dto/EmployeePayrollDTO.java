686752394cc46ea5552662f4b07f59bf5f482fd1
SaiDeeksha29 <saideeksha29@gmail.com>
2020-12-13 23:48:14 +0530

    Added:Validation to name field and salary field

--- src/main/java/com/example/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/example/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -1,8 +1,14 @@
 package com.example.employeepayrollapp.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
+
 public class EmployeePayrollDTO {
 
+	@Pattern(regexp = "^[A-Z]{1}[A-Za-z\\s]{2,}$", message = "Employee name Invalid")
 	public String name;
+
+	@Min(value = 500, message = "Minimum Wage should be more than 500")
 	public long salary;
 
 	public EmployeePayrollDTO(String name, long salary) {
